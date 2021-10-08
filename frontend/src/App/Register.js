55daee5929249121e08c2f6ffe73a7bab6333099
Gabriel Borges de Carvalho <gabriel.carvalho@alkent.com.br>
2021-10-07 22:44:19 -0300

    Criação da tela dos registros de usuários e  validação de login

--- frontend/src/App/Register.js
+++ frontend/src/App/Register.js
@@ -5,7 +5,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box'
+import Box from '@mui/material/Box';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -21,22 +26,33 @@ function Copyright(props) {
 
 const theme = createTheme();
 
-const Login = props => {
+const CreateLogin = props => {
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [teacher, setTeacher] = useState("")
 
   const handleSubmit = async (event) => {
-    if(!email || !password ) {
+    if(!email || !name || !password || !teacher) {
       alert("Preencha todos os campos")
       return;
     }
+
     const result = await verifyLoginInApi(email, password)
+
     if(result.status === 200){
+      alert("Você já possui uma conta!")
       localStorage.setItem("isAuthenticated",result.data.token)
       props.history.push("/")
+      return;
+    }
+    const create = await registerUserInApi(name,email,password, teacher)
+    if(create.status === 200){
+      alert("Conta criada com sucesso")
+      props.history.push("/login")
     }else{
-      alert("Dados incorretos");
+      alert("Dados incorretos, por favor revise-os")
     }
   };
 
@@ -44,6 +60,17 @@ const Login = props => {
     return await axios.post("http://localhost:3000/auth/login",{
       email: email,
       password: password
+    })
+    .then(response => response)
+    .catch(error => error)
+  }
+
+  const registerUserInApi = async(name,email,password, teacher) =>{
+    return await axios.post("http://localhost:3000/auth/register",{
+      name: name,
+      email: email,
+      password: password,
+      is_teacher: teacher
     })
     .then(response => response)
     .catch(error => error)
@@ -61,12 +88,24 @@ const Login = props => {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             </Avatar>
           <Typography component="h1" variant="h5">
-            Login
+            Registrar
           </Typography>
           <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="name"
+              label="Nome"
+              type="name"
+              id="name"
+              autoComplete="text"
+              color="secondary"
+              onChange={(e) => setName(e.target.value)}
+            />
             <TextField
               margin="normal"
               required
@@ -76,6 +115,7 @@ const Login = props => {
               name="email"
               autoComplete="email"
               autoFocus
+              color="secondary"
               onChange={(e) => setEmail(e.target.value)}
             />
             <TextField
@@ -87,20 +127,29 @@ const Login = props => {
               type="password"
               id="password"
               autoComplete="current-password"
+              color="secondary"
               onChange={(e) => setPassword(e.target.value)}
             />
+            <FormControl component="fieldset" required>
+              <FormLabel component="legend" color="secondary">Você é um professor?</FormLabel>
+              <RadioGroup row aria-label="professor" name="row-radio-buttons-group" onChange={(e) => setTeacher(e.target.value)}>
+                <FormControlLabel value={false} control={<Radio color="secondary" />} label="Não"/>
+                <FormControlLabel value={true} control={<Radio color="secondary" />} label="Sim"/>
+              </RadioGroup>
+            </FormControl>
             <Button
               fullWidth
+              color="secondary"
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
               onClick={handleSubmit}
             >
-              Entrar
+              Registrar
             </Button>
             <Grid container>
               <Grid item>
-                <Link href="/create-login" variant="body2">
-                  {"Não tem uma conta?"}
+                <Link href="/login" variant="body2" color="secondary">
+                  {"Já possui conta?"}
                 </Link>
               </Grid>
             </Grid>
@@ -112,4 +161,4 @@ const Login = props => {
   );
 }
 
-export default Login
+export default CreateLogin
--- frontend/src/Routes/routes.js
+++ frontend/src/Routes/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
-import Login from "../App/Login"
+import Login from "../App/Login";
+import Register from "../App/Register";
 import Home from "../App/Home";
 import ProtectedRoute from "./ProtectedRoute";
 const Routes = () => {
@@ -8,6 +9,7 @@ const Routes = () => {
         <BrowserRouter>
             <Switch>
                 <Route component = { Login }  path="/login"/>
+                <Route component = { Register }  path="/create-login"/>
                 <ProtectedRoute component = { Home }  path="/" exact/>
             </Switch>
         </BrowserRouter>
